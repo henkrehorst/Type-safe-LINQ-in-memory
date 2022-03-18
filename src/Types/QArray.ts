161080ac20676f54d1e8916322e7886abd8587cf
mark <mark.hof123@gmail.com>
2022-03-18 10:50:30 +0100

    select with test

--- src/Types/QArray.ts
+++ src/Types/QArray.ts
@@ -1,17 +1,16 @@
 export type QArray<T> = {
     data: Array<T>
-    select: <K extends keyof T>(...props: K[]) => Array<Extract<T, K[]>>
+    select: <K extends keyof T>(...props: K[]) => Array<Pick<T,typeof props[number]>>
 }
 
 export const qArray = <T>(initData: Array<T>): QArray<T> => ({
     data: initData,
-    select: function <K extends keyof T>(...params: K[]) {
-        let array: Array<Extract<T, K[]>> = []
-        
+    select: function <K extends keyof T>(...props: K[]) {
+        let array: Array<Pick<T,typeof props[number]>> = []
         this.data.forEach((item: T) => {
             let result: any = {}
-            params.forEach((param: K) => {result[param] = item[param]})
-            array.push(result as Extract<T, K[]>)
+            props.forEach((prop: K) => {result[prop] = item[prop]})
+            array.push(result as Pick<T, typeof props[number]>)
         });
         return array
     }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,22 @@
 import {expect} from "chai";
 import {timesTwo} from "../src";
+import {qArray} from "../src/Types/QArray"
+import {student1, student2, student3} from "../src/Data/Data"
 
 describe('Check check', () => {
     it('check', () => {
         expect(timesTwo(2)).to.equal(4)
+    })
+})
+
+describe('Select testing', () => {
+    it('', () => {
+        const students = [student1,student2,student3]
+
+        let studentArray = qArray(students)
+
+        let selectedStudentArray = studentArray.select("FirstName", "StudentNumber")
+
+        expect(Object.keys(selectedStudentArray[0]) as Array<keyof typeof selectedStudentArray[0]>).to.equal(["FirstName","StudentNumber"])
     })
 })
