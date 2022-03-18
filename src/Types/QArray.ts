d80720ef90ee89ab291c4a4a320b359ee386abec
henk rehorst <henkrehorst@outlook.com>
2022-03-18 12:45:50 +0100

    added orderBy function

--- src/Types/QArray.ts
+++ src/Types/QArray.ts
@@ -1,6 +1,7 @@
 export type QArray<T> = {
     data: Array<T>
     select: <K extends keyof T>(...props: K[]) => Array<Pick<T,typeof props[number]>>
+    orderBy: <K extends keyof T>(property: K, order?: "asc" | "desc") => T[]
 }
 
 export const qArray = <T>(initData: Array<T>): QArray<T> => ({
@@ -13,5 +14,23 @@ export const qArray = <T>(initData: Array<T>): QArray<T> => ({
             array.push(result as Pick<T, typeof props[number]>)
         });
         return array
+    },
+    orderBy: function <K extends keyof T>(property: K, order: 'asc' | 'desc' = 'asc') {
+        return initData.sort((a, b) => {
+            if(order === 'desc') [a, b] = [b, a];
+
+            switch (typeof a[property]) {
+                case "boolean":
+                    return Boolean(a[property]) === Boolean(b[property]) ? 0 : Boolean(b[property]) ? 1: -1;
+                case "number":
+                    return Number(a[property]) - Number(b[property]);
+                case "string":
+                    const lowerA = String(a[property]).toLowerCase(),
+                        lowerB = String(b[property]).toLowerCase();
+                    return lowerA < lowerB ? -1 : lowerA > lowerB ? 1 : 0;
+            }
+
+            return 0;
+        })
     }
 })
